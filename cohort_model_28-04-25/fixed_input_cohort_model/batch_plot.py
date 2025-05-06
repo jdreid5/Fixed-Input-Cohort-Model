6d72e4e081b39963d64e5e9f7b675038bdcd8b10
jdreid5 <jdreid5@aol.com>
2025-05-06 14:58:45 +0100

    parallelise python script

--- cohort_model_28-04-25/fixed_input_cohort_model/batch_plot.py
+++ cohort_model_28-04-25/fixed_input_cohort_model/batch_plot.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
+from concurrent.futures import ProcessPoolExecutor
+
 files = [
     ('Initial_SOC.csv', 'Initial SOC'),
     ('absolute_SOC_2050.csv', 'Absolute SOC 2050'),
@@ -13,25 +15,33 @@ files = [
     ('cumulative_tree_carbon_2080.csv', 'Cumulative Tree Carbon 2080'),
 ]
 
-for fname, title in files:
+def plot_one(args):
+    fname, title = args
     data = np.loadtxt(fname, delimiter=',')
-    x = data[:,0]; y = data[:,1]; vals = data[:,2]
+    x, y, vals = data[:,0], data[:,1], data[:,2]
     sf = 1000.0
-    grid_shape = (1+int((y.max()-y.min())/sf), 1+int((x.max()-x.min())/sf))
-    grid = np.full(grid_shape, np.nan)
-    yi = ((y-y.min())/sf).astype(int)
-    xi = ((x-x.min())/sf).astype(int)
-    np.clip(yi, 0, grid_shape[0]-1, out=yi)
-    np.clip(xi, 0, grid_shape[1]-1, out=xi)
+    rows = 1 + int((y.max()-y.min())/sf)
+    cols = 1 + int((x.max()-x.min())/sf)
+    grid = np.full((rows, cols), np.nan)
+    yi = ((y - y.min())/sf).astype(int)
+    xi = ((x - x.min())/sf).astype(int)
+    np.clip(yi, 0, rows-1, out=yi)
+    np.clip(xi, 0, cols-1, out=xi)
     grid[yi, xi] = vals
     fig, ax = plt.subplots()
     ax.set_axis_off()
-    im = ax.imshow(np.ma.masked_invalid(grid), origin='lower', interpolation='nearest', cmap='gist_ncar')
-    cbar = plt.colorbar(im, shrink=0.5)
+    img = ax.imshow(np.ma.masked_invalid(grid), origin='lower', interpolation='nearest', cmap='gist_ncar')
+    cbar = plt.colorbar(img, shrink=0.5)
     ax.set_title(title)
-    cbar.set_label('SOC (t C ha$^{-1}$)', rotation=270, labelpad=15)
+    cbar.set_label('SOC (t C ha^-1)', rotation=270, labelpad=15)
     ax.xaxis.set_major_formatter(FuncFormatter(lambda x, _: f'{int(x)}'))
     ax.yaxis.set_major_formatter(FuncFormatter(lambda y, _: f'{int(y)}'))
-    out = title.replace(' ','_') + '.png'
-    fig.savefig(out, dpi=500, bbox_inches='tight')
+    out = title.replace(' ', '_') + '.png'
+    fig.savefig(out, dpi=500)
     plt.close(fig)
+    return out
+
+if __name__ == '__main__':
+    with ProcessPoolExecutor() as executor:
+        for output_path in executor.map(plot_one, files):
+            print(f'Wrote {output_path}')
